e406c9cb6403c09df9fc2c86e8f47d076b8bba08
TheBigH50 <HFangmeier@outlook.com>
2022-09-01 14:04:58 -0500

    splice for removal

--- src/wishlist.js
+++ src/wishlist.js
@@ -1,8 +1,24 @@
 import Car from "./car.js";
+export default WishList;
 
 class WishList {
-  constructor(){
-    this.list = [];
-    this.nextId = 0;
-};
+  //two class properties
+    list = [];
+    nextId = 0;
+
+    add(make, model, year) {
+      //use the Car class to create a new car
+      let car = new Car(this.nextId++, make, model, year)
+      //add that car to the wishlist list
+      this.list.push(car);
+    }
+    remove(carId) {
+      //find the car in the wishlist list that matches the remove id
+      let index = this.list.findIndex((car) => car.id == carId);
+      //remove it and update wishlist list
+      this.list.splice(index, 1);
+    }
+
 }
+
+
